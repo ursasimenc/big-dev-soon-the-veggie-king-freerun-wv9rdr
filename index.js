21dcd76768ef2cdc9660b88ad29e440af07a7aff
ursasimenc <ursasimenc17@gmail.com>
2023-11-07 23:54:34 +0100

    completed hero section

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+const heroImgOnePath = "./assets/images/hero-section-slide-food-1.png";
+const heroImgTwoPath = "./assets/images/hero-section-slide-food-2.png";
+const heroImgThreePath = "./assets/images/hero-section-slide-food-3.png";
+const heroImgFourPath = "./assets/images/hero-section-slide-food-4.png";
+const heroImg = document.getElementById("hero-img");
+
+let heroImgIndex = 0;
+const heroImgPaths = [heroImgOnePath, heroImgTwoPath, heroImgThreePath, heroImgFourPath];
+setInterval(() => {
+	heroImgIndex = (heroImgIndex + 1) % heroImgPaths.length;
+	heroImg.src = heroImgPaths[heroImgIndex];
+}, 3000);
